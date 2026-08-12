7fcd2c1697f31bad990cead8d52060ffdf9800bd
muhammadrizwan0991-commits <muhammadrizwan0991@gmail.com>
2026-08-12 14:09:57 +0000

    second code

---
+++ 01basics/second.js
@@ -0,0 +1,21 @@
+const accountId = 123;
+let accountEmail = "Hello@gmail.com";
+var accountPassword = '145';
+let accountState;
+accountCity = 'Hyderabad';
+
+accountPassword = '1234567890';
+
+// accountId = 23;
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope.
+
+*/
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+
