02d96f81f69824497ba160416f9c0f22a00c1cad
muhammadrizwan0991-commits <muhammadrizwan0991@gmail.com>
2026-08-12 14:28:32 +0000

    data type

---
+++ 01basics/third.js
@@ -0,0 +1,20 @@
+"use strict"// treat all JS code as newer version
+
+// alert(3 + 3); // we can't use this in new version of node now a days it only supports in browser console or html format there is another syntax to use this in node but we'll see that in future.
+
+
+// data type = numbers => 2 to power 53 
+// bigInt
+// string => ""
+// boolean => true or false
+// null  => stand alone value
+// undefined => 
+// symbol => unique
+
+// object
+
+// null => object typeof null = object
+// undefined => undefined typeof undefined = type;
+let age = 15;
+console.log(typeof "Fahad");
+console.table([typeof age, typeof "Fahad", typeof true, typeof null, typeof 55]);
